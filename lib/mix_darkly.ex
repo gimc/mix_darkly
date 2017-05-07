69b4fb88ed78ef586fc052f926107c9cd86958ff
Gordon McAllister <gordonmc8@gmail.com>
2017-05-07 11:50:45 +0100

    Created application skeleton.

---
+++ lib/mix_darkly.ex
@@ -0,0 +1,18 @@
+defmodule MixDarkly do
+  @moduledoc """
+  Documentation for MixDarkly.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixDarkly.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_darkly_test.exs
@@ -0,0 +1,8 @@
+defmodule MixDarklyTest do
+  use ExUnit.Case
+  doctest MixDarkly
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
