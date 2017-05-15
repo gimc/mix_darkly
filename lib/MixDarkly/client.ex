40da1c9a96a5f048118b98fa1240b19b381f7326
Gordon McAllister <gordonmc8@gmail.com>
2017-05-15 21:42:33 +0100

    Reinstated eval_flag logic and fixed tests.

--- lib/MixDarkly/client.ex
+++ lib/MixDarkly/client.ex
@@ -1,11 +1,18 @@
 defmodule MixDarkly.Client do
   require Logger
 
+  alias MixDarkly.Evaluation
   alias MixDarkly.FeatureFlag
   alias MixDarkly.FeatureStore
   alias MixDarkly.UpdateProcessor
 
-  @type t :: %{
+  defstruct sdk_key: nil,
+            config: %MixDarkly.Config{},
+            event_processor: nil,
+            update_processor: nil,
+            feature_store: nil
+
+  @type t :: %MixDarkly.Client{
     :sdk_key => String.t(),
     :config => MixDarkly.Config.t(),
     :event_processor => pid,
@@ -14,11 +21,13 @@ defmodule MixDarkly.Client do
   }
 
   @spec variation(client :: Client.t(), key :: String.t(), user :: User.t(), default :: term) ::
-    {:ok, value :: term} |
-    {:error, reason :: String.t()}
+    {:ok, value :: term}
   def variation(%{config: %{offline: true}}, _key, _user, default), do: {:ok, default}
-  def variation(_client, _key, _user, default) do
-    {:ok, default}
+  def variation(client, key, user, default) do
+    case evaluate(client, key, user, default) do
+      {:ok, value, version} -> {:ok, value}
+      {:error, reason} -> {:ok, default}
+    end
   end
 
   @spec bool_variation(client :: Client.t(), key :: String.t(), user :: User.t() , default :: boolean) ::
@@ -57,22 +66,21 @@ defmodule MixDarkly.Client do
     end
   end
 
-  @spec eval_flag(client :: Client.t(), flag :: FeatureFlag.feature_flag(), user :: User.t()) ::
+  @spec eval_flag(client :: Client.t(), flag :: FeatureFlag.t(), user :: User.t()) ::
     {:ok, {value :: term, pre_requisite_events :: []}} |
     {:error, {reason :: String.t(), pre_requisite_events :: []}}
   def eval_flag(client, flag, user) do
-    # cond do
-    #   flag.on ->
-    #     {result, evaluation} = Evaluation.for_user(user, flag, client.store)
-    #     cond do
-    #       result == :error -> {:error, {"", evaluation.pre_requisite_events}}
-    #       evaluation.value != nil -> {:ok, {evaluation.value, evaluation.pre_requisite_events}}
-    #     end
-    #   flag.off_variation != nil and flag.off_variation < length(flag.variations) ->
-    #     {:ok, Enum.at(flag.variations, flag.off_variation), evaluation.pre_requisite_events}}
-    #   true ->
-    #     {:error, {"", evaluation.pre_requisite_events}}
-    # end
-    {:ok, {true, []}}
+    cond do
+      flag.on ->
+        {result, evaluation} = Evaluation.evaluate_explain(flag, user, client.feature_store)
+        cond do
+          result == :error -> {:error, {"", evaluation.prerequisite_request_events}}
+          evaluation.value != nil -> {:ok, {evaluation.value, evaluation.prerequisite_request_events}}
+        end
+      flag.off_variation != nil and flag.off_variation < length(flag.variations) ->
+        {:ok, {Enum.at(flag.variations, flag.off_variation), []}}
+      true ->
+        {:error, {"Flag is not enabled and no off variation is set", []}}
+    end
   end
 end
--- test/mix_darkly/client_test.exs
+++ test/mix_darkly/client_test.exs
@@ -2,27 +2,64 @@ defmodule MixDarkly.ClientTest do
   use ExUnit.Case
 
   alias MixDarkly.Client, as: Sut
+  alias MixDarkly.FeatureFlag
   alias MixDarkly.FeatureStore
   alias MixDarkly.UpdateProcessor
+  alias MixDarkly.User
 
-  test "Returns bool variation for valid key" do
-    client = nil
-    user = nil
+  defp setup do
+    {:ok, update_processor} = UpdateProcessor.start_link()
+    UpdateProcessor.initialize(update_processor)
+
+    {:ok, feature_store} = FeatureStore.start_link()
+
+    client = %Sut{
+      update_processor: update_processor,
+      feature_store: feature_store
+    }
+
+    user = %User{
+      key: "User1",
+      ip: "127.0.0.1"
+    }
+
+    [client: client, user: user]
+  end
+
+  test "Returns bool variation for valid key", context do
+    context = setup()
+
+    flag = %FeatureFlag{
+      key: "BooleanFlag",
+      rules: [%{
+        clauses: [%{
+          attribute: "ip", op: "eq", values: ["127.0.0.1"], negate: false
+        }],
+        variation_or_rollout: %{variation: 1, rollout: nil}
+      }],
+      variations: [false, true],
+      fallthrough: %{variation: 0, rollout: nil}
+    }
+
+    FeatureStore.put(context[:client].feature_store, flag)
+
     default_value = false
     expected_value = true
 
-    {:ok, value} = Sut.bool_variation(client, "key", user, default_value)
+    {:ok, value} = Sut.bool_variation(context[:client], "BooleanFlag", context[:user], default_value)
 
     assert value == expected_value
   end
 
   test "Returns default bool variation for non-existing key" do
-    {:ok, value} = Sut.bool_variation(nil, "idontexist", nil, true)
+    context = setup()
+    {:ok, value} = Sut.bool_variation(context[:client], "idontexist", context[:user], true)
     assert value == true
   end
 
   test "Using bool_variation to return non-boolean value returns error" do
-    {result, reason} = Sut.bool_variation(nil, "idontexist", nil, "i am not a bool")
+    context = setup()
+    {result, reason} = Sut.bool_variation(context[:client], "idontexist", context[:user], "i am not a bool")
     assert result == :error
     assert String.starts_with?(reason, "Incompatible type")
   end
@@ -58,13 +95,23 @@ defmodule MixDarkly.ClientTest do
   test "evaluate returns true when feature flag exists" do
     {:ok, update_processor} = UpdateProcessor.start_link()
     {:ok, feature_store} = FeatureStore.start_link()
-    client = %{update_processor: update_processor,
-               feature_store: feature_store,
-               config: %{offline: false, use_ldd: false}}
-    user = %{key: "uniqueid"}
+
+    client = %Sut{update_processor: update_processor,
+                  feature_store: feature_store,
+                  config: %{
+                    offline: false, use_ldd: false
+                  }}
+
+    user = %User{key: "uniqueid"}
+
+    flag = %FeatureFlag{key: "foo",
+                        version: 2,
+                        on: true,
+                        variations: [true, false],
+                        targets: [%{values: ["uniqueid"], variation: 0}]}
 
     UpdateProcessor.initialize(update_processor)
-    FeatureStore.put(feature_store, %{key: "foo", version: 2, on: true, variations: [true, false]})
+    FeatureStore.put(feature_store, flag)
 
     {:ok, value, version} = Sut.evaluate(client, "foo", user, false)
 
